ae63e8a605b698e9997ad52076fa41bd68309506
piotrekhasiec <piotrekhasiec@gmail.com>
2021-12-19 23:43:18 +0100

    ustawiłem name na charfield, usunąłem niepotrzebne widoki i obrazek na niepotrzebny i działa

--- Indi_IO/urls.py
+++ Indi_IO/urls.py
@@ -4,12 +4,12 @@ from django.contrib.auth.views import LoginView
 from django.urls import path, include
 
 
-from users.views import index 
+#from users.views import index 
 from users.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("users.urls")),
-    path('projekt/<id>/',projects, name = 'projekt'),
+  #  path('projekt/<id>/',projects, name = 'projekt'),
     path('login/',LoginView.as_view(),name = "login"),
     path('',include('account.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -6,38 +6,6 @@ from .models import Projects
 from .forms import  LoginForms
 from django.contrib.auth.decorators import login_required
 
-@login_required
-def dashboard(request):
-    return HttpResponse("aaa")
-# Create your views here.
-def index(request):
-    Projekty =Projects.objects.all()
-    dane = {'Projekty':Projekty}
-    return render(request,"index.html",dane)
 
-
-def projects (request,id):
-    projekt_user = Projects.objects.get(pk = id)
-    
-    return JsonResponse(projekt_user)
-
-def user_login(request):
-    if request.method == "POST":
-        form = LoginForms(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username = cd['username'],password = cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request,user)
-                    return HttpResponse("Ok")
-                else:
-                    return HttpResponse("Konto zablokowane")
-        else:
-            return HttpResponse("Nieprawidłowe dane")
-    else:
-        form = LoginForms()
-
-    return render(request, "login.html",{'form':form})
 
 
