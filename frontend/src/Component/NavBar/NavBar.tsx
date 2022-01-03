3bc2110b669040eb5051ec20150e430cf7597287
InKronos <i15.piorkowski@gmail.com>
2022-01-04 00:03:02 +0100

    Session with login works!

--- frontend/src/Component/NavBar/NavBar.tsx
+++ frontend/src/Component/NavBar/NavBar.tsx
@@ -1,26 +1,56 @@
-import React, { Component } from 'react';
-import "./NavBar.css"
-import { Button } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../../Actions/auth';
+import { Nav } from 'reactstrap';
+
+const NavBar = ({logout, isAuthenticated}) => {
+    
+    const [redirect, setRedirect] = useState(false);
+
+    const logout_user = () => {
+        logout();
+        setRedirect(true);
+    };
+
+    const guestLinks = () => (
+        <Fragment>
+            <Link to="/login" style={{ textDecoration: 'none' }}>
+                        <button type="button" className='btn'>Sign in</button>
+            </Link>
+            <button type="button" className='btn'>Sign up</button>
+        </Fragment>
+    );
+
+    const authLinks = () => (
+        <a className='nav-link' href='#!' style={{ textDecoration: 'none' }} onClick={logout_user}>
+            <button type="button" className='btn'>Logout</button>
+        </a>
+    );
 
 
-class NavBar extends Component{
-    render(){
-        return(
+    return(
+        <div>
             <Link to="/" style={{ textDecoration: 'none' }}>
                 <div className="navbar" id="main">
                     <div id="title">
                         <span>Projects</span>  
                         <span>.io</span>
                     </div>
-                    <Link to="/login" style={{ textDecoration: 'none' }}>
-                        <button type="button" className='btn'>Sign in</button>
-                    </Link>
-                    <button type="button" className='btn'>Sign up</button>
+                    
                 </div>
             </Link>
-        );
-    }
+            {isAuthenticated ? authLinks() : guestLinks()}
+        </div>
+    )
+        
+        
+
+
 }
 
-export default NavBar;
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, {logout})(NavBar);
