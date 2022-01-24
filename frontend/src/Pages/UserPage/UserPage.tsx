5909e2efce38f68b33408c495cf8fbc1986584d5
InKronos <i15.piorkowski@gmail.com>
2022-01-24 14:17:51 +0100

    user delete

--- frontend/src/Pages/UserPage/UserPage.tsx
+++ frontend/src/Pages/UserPage/UserPage.tsx
@@ -3,18 +3,61 @@ import { Route, Navigate, Link } from 'react-router-dom';
 import NavBar from "../../Component/NavBar/NavBar";
 import "./UserPage.css"
 import { connect } from "react-redux";
+import CustomPopup from "../../Component/CustomPopup/CustomPopup";
 
 const UserPage = ({ }) => {
 
     const [profile, setProfile] = useState([]);
+    const [projects, setProjects] = useState([]);
     const [loadingProfile, setLoadingProfile] = useState(true);
-    const [error, setError] = useState(false);
+    const [loadingProjects, setLoadingProjects] = useState(true);
+    const [error1, setError1] = useState(false);
+    const [error2, setError2] = useState(false);
+    const [visibility, setVisibility] = useState(false);
+
+    const [formData, setFormData] = useState({
+        password: ''
+    });
+    const { password } = formData;
+    
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const deleteProfile = e => {
+        e.preventDefault();
+        deleteProfileFun(password);
+    }
+
     const location = window.location.pathname.split("/");
 
     useEffect(() => {
         getProfile();
+        getProjects();
     }, []);
 
+    const popupDeleteCloseHandler = (e) => {
+        setVisibility(e);
+    };
+
+
+    const deleteProfileFun = (current_password) => {
+        return fetch(`${process.env.REACT_APP_REMOTE_URL}/auth/users/me/`, {
+            method: 'DELETE',
+            mode: 'cors',
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': `JWT ${localStorage.getItem('access')}`
+            },
+            body: JSON.stringify({ current_password })
+        })
+          .then(response => response.json())
+          .then(responseJson => {
+                localStorage.removeItem('access');
+                localStorage.removeItem('refresh');
+          })
+          .catch(error => {
+           
+          });
+    }
 
     const getProfile = () => {
         return fetch(`${process.env.REACT_APP_REMOTE_URL}/Users/api/Users/:id/`.replace(":id", location[2]), {
@@ -32,13 +75,34 @@ const UserPage = ({ }) => {
           })
           .catch(error => {
             setLoadingProfile(false);
-            setError(true);
+            setError1(true);
+          });
+    }
+
+
+    const getProjects = () => {
+        return fetch(`${process.env.REACT_APP_REMOTE_URL}/Projects/api/Projects/myProjects/`, {
+            method: 'GET',
+            mode: 'cors',
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': `JWT ${localStorage.getItem('access')}`
+            }
+        })
+          .then(response => response.json())
+          .then(responseJson => {
+            setProjects(responseJson);
+            setLoadingProjects(false);
+          })
+          .catch(error => {
+            setLoadingProjects(false);
+            setError2(true);
           });
     }
 
     return (
         <div>
-            { !loadingProfile && !error && profile.map(profile =>
+            { !loadingProfile && !error1 && profile.map(profile =>
             <div id="Widget">
             
             <div id="Profile-card">
@@ -82,7 +146,19 @@ const UserPage = ({ }) => {
                     </Link>
 
                     <button type="button" className="btn">Become developer</button>
-                    <button type="button" className="btn">Delete profile</button>
+                    <button type="button" className="btn" onClick={(e) => setVisibility(!visibility)}>Delete profile</button>
+                    <CustomPopup
+                    onClose={popupDeleteCloseHandler}
+                    show={visibility}
+                    title="Delete Profile"
+                    >
+                        <h2>Type password to delete</h2>
+                        <form onSubmit={e => deleteProfile(e)}>
+                            <input type="password" name="password" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon2"  value={password}
+                            onChange={e => onChange(e)}/>
+                            <button type="submit" className="btn">YES</button>
+                        </form>
+                    </CustomPopup>
                 </div>
             }
         </div>
